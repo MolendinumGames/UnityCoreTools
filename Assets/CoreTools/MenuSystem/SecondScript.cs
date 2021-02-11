8dac2e655f9317717f79d62373e63e8c9367d4c1
Sundiray <61310509+Sundiray@users.noreply.github.com>
2021-02-11 16:59:30 +0100

    testing cleanup - deleted contents of menusystem folder

--- Assets/CoreTools/MenuSystem/SecondScript.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SecondScript : MonoBehaviour
-{
-    // This is just for testing merging conflicts
-}
