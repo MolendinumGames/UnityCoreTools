5d5962ad2ef42851696f1b6b99dd5e8acd642ccb
Sundiray <61310509+Sundiray@users.noreply.github.com>
2021-02-24 22:32:34 +0100

    Added UserSettingsController

---
+++ Assets/CoreTools/UserSettingsController.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace CoreTools
+{
+    public class UserSettingsController : Singleton<UserSettingsController>
+    {
+        protected override bool Persistent => true;
+
+        [Header("Audio")]
+        [SerializeField]
+        AudioMixer masterMixer;
+        [SerializeField]
+        Vector2Int minMaxVolumeLevel = new Vector2Int(-80, 0);
+
+        private readonly string volumeId = "volume";
+        private readonly string vSyncId = "vsync";
+        private readonly string fullscreenId = "fullscreen";
+        private readonly string resolutionId = "resolution";
+
+        private void Awake()
+        {
+            LoadSettings();
+        }
+        public void SaveSettings()
+        {
+            float volume;
+            masterMixer.GetFloat("volume", out volume);
+            PlayerPrefs.SetFloat(volumeId, volume);
+            PlayerPrefs.SetInt(vSyncId, QualitySettings.vSyncCount);
+            PlayerPrefs.SetString(resolutionId, Screen.currentResolution.ToString());
+            PlayerPrefs.SetString(fullscreenId, Screen.fullScreen.ToString());
+
+        }
+        public void LoadSettings()
+        {
+            LoadVolume();
+
+            LoadVSync();
+
+            LoadScreenSettings();
+        }
+        private void LoadVolume()
+        {
+            if (PlayerPrefs.HasKey(volumeId))
+            {
+                float volume = PlayerPrefs.GetFloat(volumeId);
+                SetVolume(volume);
+            }
+        }
+        private void LoadVSync()
+        {
+            if (PlayerPrefs.HasKey(vSyncId))
+            {
+                int vSync = PlayerPrefs.GetInt(vSyncId);
+                SetVSync(vSync);
+            }
+        }
+        private void LoadScreenSettings()
+        {
+            if (PlayerPrefs.HasKey(resolutionId) && PlayerPrefs.HasKey(fullscreenId))
+            {
+                bool fullScreen = true;
+                if (!bool.TryParse(PlayerPrefs.GetString(fullscreenId), out fullScreen))
+                    Debug.LogError($"Could not read fullscreen from Playerprefs. Was {PlayerPrefs.GetString(fullscreenId)}");
+
+                string newResText = PlayerPrefs.GetString(resolutionId);
+                string[] splitRes = newResText.Split('x');
+                int width, height, refreshRate;
+                if (!int.TryParse(splitRes[0], out width))
+                {
+                    Debug.LogError($"Could not read width resolution from Playerprefs. Was: {newResText}");
+                    return;
+                }
+                string[] secondSplitRes = splitRes[1].Split('@');
+                if (!int.TryParse(secondSplitRes[0], out height))
+                {
+                    Debug.LogError($"Could not read height resolution from Playerprefs. Was: {newResText}");
+                    return;
+                }
+                if (!int.TryParse(secondSplitRes[1], out refreshRate))
+                {
+                    Debug.LogError($"Could not read Hz from Playerprefs. Was: {newResText}");
+                    return;
+                }
+
+                SetScreen(width, height, fullScreen, refreshRate);
+            }
+        }
+
+        #region Public Set Methods
+        public void SetVSync(int value)
+        {
+            int vSync = Mathf.Clamp(value, 0, 4);
+            QualitySettings.vSyncCount = vSync;
+        }
+        public void SetFullscreen(bool state)
+        {
+            Screen.fullScreen = state;
+        }
+        public void SetScreenresolution(int width, int height)
+        {
+            bool fullScreen = Screen.fullScreen;
+            Screen.SetResolution(width, height, fullScreen);
+        }
+        public void SetScreen(int width, int height, bool fullScreen, int refreshRate)
+        {
+            Screen.SetResolution(width, height, fullScreen, refreshRate);
+        }
+        public void SetRefreshRate(int rate)
+        {
+            int width = Screen.width;
+            int heigth = Screen.height;
+            bool fullScreen = Screen.fullScreen;
+            rate = Mathf.Abs(rate);
+            Screen.SetResolution(width, heigth, fullScreen, rate);
+        }
+        public void SetVolume(float level)
+        {
+            int min = minMaxVolumeLevel.x;
+            int max = minMaxVolumeLevel.y;
+            level = Mathf.Clamp(level, min, max);
+            masterMixer.SetFloat("volume", level);
+        }
+        #endregion
+    }
+}
