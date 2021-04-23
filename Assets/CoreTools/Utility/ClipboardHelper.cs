583e6f3af2cefac1c906a296710d2eeb7b67dcfd
DESKTOP-TPU73AF\chris <christoph.roemer@yahoo.de>
2021-04-23 19:19:02 +0200

    Added comments to clipboard helper

--- Assets/CoreTools/Utility/ClipboardHelper.cs
+++ Assets/CoreTools/Utility/ClipboardHelper.cs
@@ -1,21 +1,27 @@
 using System;
-using System.Reflection;
 using UnityEngine;
 
 public static class ClipboardHelper
 {
-    public static string ReadClipboard()
-    {
-        return GUIUtility.systemCopyBuffer;
-    }
+    /// <returns> The systems clipboard as string</returns>
+    public static string ReadClipboard() => GUIUtility.systemCopyBuffer;
+
+    /// <summary>
+    /// Copy a string to system wide clipboard
+    /// </summary>
     public static void SetClipboard(string value)
     {
         if (string.IsNullOrEmpty(value))
         {
-            Debug.LogWarning($"Tried to copy an empty string to clipboard. String: {value}");
+            Debug.LogWarning($"Copied an empty string to clipboard.");
+            GUIUtility.systemCopyBuffer = "";
             return;
         }
         GUIUtility.systemCopyBuffer = value;
     }
+
+    /// <summary>
+    /// Copy a string to system wide clipboard
+    /// </summary>
     public static void CopyToClipboard(this string value) => SetClipboard(value);
 }
