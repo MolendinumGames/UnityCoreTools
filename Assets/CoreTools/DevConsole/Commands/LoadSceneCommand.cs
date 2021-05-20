84403e0da6c0ce5d9b825a8bbc5c164c0fc2186d
DESKTOP-TPU73AF\chris <christoph.roemer@yahoo.de>
2021-05-20 13:48:02 +0200

    small rename/refactor in LoadSceneCommand

--- Assets/CoreTools/DevConsole/Commands/LoadSceneCommand.cs
+++ Assets/CoreTools/DevConsole/Commands/LoadSceneCommand.cs
@@ -5,13 +5,13 @@ namespace CoreTools.Console
 {
     public class LoadSceneCommand : ConsoleCommand
     {
-        readonly string wrongInputMsg = "This command takes only one input.";
+        readonly string wrongAmountMsg = "This command takes only one input.";
         readonly string notFoundMsg = "No scene with that index or name found";
-        string wrongMessage = "";
+        string wrongInputMsg = "";
 
         public override string Command => "loadscene";
 
-        public override string[] WrongInputMessage => new string[] { wrongMessage };
+        public override string[] WrongInputMessage => new string[] { wrongInputMsg };
 
         public override string[] SuccessMessage => null;
 
@@ -19,7 +19,7 @@ namespace CoreTools.Console
         {
             if (args.Length != 1)
             {
-                wrongMessage = wrongInputMsg;
+                wrongInputMsg = wrongAmountMsg;
                 return false;
             }
             else if (Int32.TryParse(args[0], out int index) 
@@ -33,7 +33,7 @@ namespace CoreTools.Console
                 SceneManager.LoadScene(args[0]);
                 return true;
             }
-            wrongMessage = notFoundMsg;
+            wrongInputMsg = notFoundMsg;
             return false;
         }
     }
