ec2d0ddd0765852a7002da346d04808dd8ee4031
Christoph <contactsundiray@gmail.com>
2021-06-24 18:25:43 +0200

    Added MenuItem to get or create a GlobalPoolManager in the scene

--- Assets/CoreTools/Pooling/GlobalPoolManager.cs
+++ Assets/CoreTools/Pooling/GlobalPoolManager.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using CoreTools;
 
 public class GlobalPoolManager : Singleton<GlobalPoolManager>
 {
     protected override bool Persistent => false;
+
+#if UNITY_EDITOR
+    [MenuItem("Tools/GetPoolManager")]
+    public static void GetOrCreatePoolManager()
+    {
+        GlobalPoolManager pool = FindObjectOfType<GlobalPoolManager>();
+        if (pool == null)
+        {
+            pool = new GameObject("PoolManager").AddComponent(typeof(GlobalPoolManager)) as GlobalPoolManager;
+        }
+        Selection.activeGameObject = pool.gameObject;
+    }
+#endif
+
 }
