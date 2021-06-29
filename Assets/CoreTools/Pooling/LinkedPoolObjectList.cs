b89acc085bf97c2d1c963e5e34870a834eef4135
Christoph <contactsundiray@gmail.com>
2021-06-29 22:58:22 +0200

    Added basic functionality LinkedList for pooling objects

---
+++ Assets/CoreTools/Pooling/LinkedPoolObjectList.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CoreTools.Pooling
+{
+    class LinkedPoolObjectList
+    {
+        // First
+        LinkedPoolObjectNode First { get; set; } = null;
+
+        // Last
+        LinkedPoolObjectNode Last { get; set; } = null;
+
+        // Empty
+        public bool IsEmpty { get => First == null; }
+
+        // Count
+        public int Count
+        {
+            get
+            {
+                if (IsEmpty)
+                    return 0;
+
+                int counter = 1;
+                var current = First;
+                while (current.Next != null)
+                {
+                    counter++;
+                    current = current.Next;
+                }
+                return counter;
+            }
+        }
+
+        // Prepend
+        public void Prepend(LinkedPoolObjectNode node)
+        {
+            if (IsEmpty)
+            {
+                SetInitialNode(node);
+            }
+            else
+            {
+                node.Next = First;
+                node.Previous = null;
+                First.Previous = node;
+                First = node;
+            }
+        }
+
+        public void PrependNew(GameObject go)
+        {
+            Prepend(new LinkedPoolObjectNode(go));
+        }
+
+        // Append
+        public void Append(LinkedPoolObjectNode node)
+        {
+            if (IsEmpty)
+            {
+                SetInitialNode(node);
+            }
+            else
+            {
+                node.Previous = Last;
+                node.Next = null;
+                Last.Next = node;
+                Last = node;
+            }
+        }
+
+        public void AppendNew(GameObject go)
+        {
+            Append(new LinkedPoolObjectNode(go));
+        }
+
+        // Clear
+        public void Clear()
+        {
+            First = null;
+            Last = null;
+        }
+
+        private void SetInitialNode(LinkedPoolObjectNode node)
+        {
+            First = node;
+            Last = node;
+        }
+
+
+    }
+}
---
+++ Assets/CoreTools/Pooling/LinkedPoolObjectNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace CoreTools.Pooling
+{
+    class LinkedPoolObjectNode
+    {
+        public LinkedPoolObjectNode(GameObject pooledObject, LinkedPoolObjectNode previous, LinkedPoolObjectNode next)
+        {
+            this.PooledObject = pooledObject;
+            this.Next = next;
+            this.Previous = previous;
+        }
+
+        public LinkedPoolObjectNode(GameObject pooledObject) : this(pooledObject, null, null)
+        { }
+
+        public LinkedPoolObjectNode() : this(null, null, null)
+        { }
+
+
+        public GameObject PooledObject { get; set; }
+
+        public LinkedPoolObjectNode Previous { get; set; }
+
+        public LinkedPoolObjectNode Next { get; set; }
+    }
+}
