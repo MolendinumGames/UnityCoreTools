5f4ff66bab749ccf19aa272dfee704668a41590c
Christoph <contactsundiray@gmail.com>
2021-07-18 00:17:52 +0200

    Added MonoB shell component for individual pools

---
+++ Assets/CoreTools/Pooling/GameObjectPoolController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoreTools.Pooling
+{
+    public class GameObjectPoolController : MonoBehaviour
+    {
+        [SerializeField]
+        bool createOnAwake = true;
+        public bool CreateOnAwake
+        {
+            get => createOnAwake;
+            set => createOnAwake = value;
+        }
+
+        [SerializeField]
+        GameObjectPool pool;
+
+        void Start()
+        {
+            if (CreateOnAwake)
+                InitializePool();
+        }
+
+        public void InitializePool() =>
+            pool.Initialize();
+
+        public GameObject GetPooledGameObject() =>
+            pool.RequestObject();
+
+        public GameObjectPool GetPool() => pool;
+    }
+}
