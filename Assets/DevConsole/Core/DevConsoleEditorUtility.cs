0a04465ff12aef4686c85f59f049fffd3ab7d12b
Christoph <contactsundiray@gmail.com>
2021-07-26 12:37:25 +0200

    Providing functionality to get dev console into the scene via editor toolbar

---
+++ Assets/DevConsole/Core/DevConsoleEditorUtility.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace CoreTools.Console
+{
+    // Provides functionality to create and select a DeveloperConsole into the currently acitve scene.
+    // If an instance of a DeveloperConsole is already in the scene, the root GameObject will be selected instead.
+    public static class DevConsoleEditorUtility
+    {
+        const string prefabPath = "DeveloperConsole";
+
+        [MenuItem("Tools/Add DeveloperConsole")]
+        public static void DevConsoleCreator()
+        {
+            // CoreTools.Console.DeveloperConsole is not a MonoBehaviour so we search for the Controller instead.
+            // The DevConsoleController sits in the root GameObject of the DevConsole GameObject. 
+            DevConsoleController devConsole = GameObject.FindObjectOfType<DevConsoleController>();
+
+            if (devConsole != null)
+            {
+                Selection.activeGameObject = devConsole.gameObject;
+            }
+            else
+            {
+                CreateNewDevConsole();
+            }
+        }
+
+        static void CreateNewDevConsole()
+        {
+            var prefab = LoadDevConsolePrefab();
+            var go = GameObject.Instantiate(prefab, Vector3.zero, Quaternion.identity, null);
+            Selection.activeGameObject = go;
+        }
+
+        static GameObject LoadDevConsolePrefab()
+        {
+            var prefab = Resources.Load<GameObject>(prefabPath);
+
+            if (prefab == null)
+                throw new System.NullReferenceException($"Developer console prefab not found in resource path: {prefabPath}");
+
+            return prefab;
+        }
+    }
+}
