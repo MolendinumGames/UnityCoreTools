b0de2d7998ff1497309ae14cd7ac570cb08a8193
Christoph <christoph.roemer@yahoo.de>
2023-01-04 20:58:25 +0100

    Updated license notice in devconsole files

--- Assets/DevConsole/Commands/ClearCommand.cs
+++ Assets/DevConsole/Commands/ClearCommand.cs
@@ -1,10 +1,11 @@
-﻿/* Copyright (c) 2021 - Christoph Römer. All rights reserved. 
+﻿/* Copyright (c) 2022 - Christoph Römer. All rights reserved. 
  * 
- * For support, feedback and suggestions please conact me under:
- * contactsundiray@gmail.com
+ * This source code is licensed under the Apache-2.0-style license found
+ * in the LICENSE file in the root directory of this source tree. 
+ * You may not use this file except in compliance with the License.
  * 
- * Check out my other content:
- * https://sundiray.itch.io/
+ * For questions, feedback and suggestions please conact me under:
+ * coretools@molendinumgames.com
  */
 
 namespace CoreTools.Console.Commands
